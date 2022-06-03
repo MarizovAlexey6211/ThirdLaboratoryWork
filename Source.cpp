9ee7db66310ff4fdcbe3bd09128a8b460d0e1c0c
MarizovAlexey6211 <mar.karnage@outlook.com>
2022-06-03 22:06:07 +0400

    print methods

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,28 @@
 #include "Road.h"
 #include <iostream>
-int main()
-{
 
-}
+	template < typename TVertex, typename TEdge>
+	void print(road_network<TVertex, TEdge> g)
+	{
+		auto begin = g.begin();
+		auto end = g.end();
+		while (begin != end) {
+			std::cout << begin->get_name() << "  (population " << begin->get_num() << ")" << std::endl;
+			{
+				auto b = begin->begin_edge();
+				auto e = begin->end_edge();
+				while (b != e) {
+					std::cout << "\n\t" << b->_get_src() << " --->     " << b->_get_dst() << " Distance::: " << b->_get_edge() << std::endl;
+					++b;
+				}
+			}
+			++begin;
+			std::cout << std::endl;
+		}
+	}
+	void print_between() {
+		std::cout << "----------------------------------------------------------------------" << std::endl;
+		std::cout << "----------------------------------------------------------------------" << std::endl;
+		std::cout << "----------------------------------------------------------------------" << std::endl;
+		std::cout << "----------------------------------------------------------------------" << std::endl;
+	}
